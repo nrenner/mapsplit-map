87d1a7a083d049481cd8ceb2dc29696b3c69d076
Norbert Renner <norbert.renner@gmx.de>
2014-03-11 21:04:52 +0100

    browserify as npm script; remove osm-pbf transform, switched to osm-read

--- build.cmd
+++
@@ -1,4 +0,0 @@
-set OSM_PBF=./node_modules/osm-pbf-leaflet/node_modules/osm-pbf
-
-call browserify . -o bundle.js %*
-call browserify -r %OSM_PBF% -e ./js/worker.js -t %OSM_PBF%/browser/transforms.js -o bundle-worker.js %*
--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-OSM_PBF=./node_modules/osm-pbf-leaflet/node_modules/osm-pbf
-
-browserify . -o bundle.js $@
-browserify -r $OSM_PBF -t $OSM_PBF/browser/transforms.js -e ./js/worker.js -o bundle-worker.js $@
