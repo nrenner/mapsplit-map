4403424e63e04ba860e27009a7f22fca4e9f985a
Norbert Renner <norbert.renner@gmx.de>
2014-03-22 18:30:11 +0100

    nightlife style

--- mapcss/styles.js
+++ mapcss/styles.js
@@ -1,5 +1,6 @@
 module.exports = {
     'default': 'mapcss/default.mapcss',
+    'nightlife': 'mapcss/nightlife.mapcss',
     'schwarzplan': 'mapcss/schwarzplan.mapcss',
     'test': 'mapcss/test.mapcss'
 };
