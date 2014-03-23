1bbae548180fef075ae39f14d8d43f593ecd280e
Norbert Renner <norbert.renner@gmx.de>
2014-03-23 12:33:46 +0100

    tourism-berlin mapcss

--- mapcss/styles.js
+++ mapcss/styles.js
@@ -1,6 +1,7 @@
 module.exports = {
     'default': 'mapcss/default.mapcss',
     'nightlife': 'mapcss/nightlife.mapcss',
+    'tourism-berlin': 'mapcss/tourism-berlin.mapcss',
     'schwarzplan': 'mapcss/schwarzplan.mapcss',
     'test': 'mapcss/test.mapcss'
 };
