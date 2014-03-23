85f4d024918cb51f3e08ded98e4519533ef3e5af
Norbert Renner <norbert.renner@gmx.de>
2014-03-23 12:44:16 +0100

    name as popup-title

--- js/popup.js
+++ js/popup.js
@@ -3,8 +3,11 @@ var getFeatureInfoHtml = function(feature) {
     var infoHtml = "<table>";
     var link = '<a href="http://www.openstreetmap.org/browse/' + feature.type + '/' + feature.id + '" target="_blank">' + feature.id + '</a>';
     infoHtml += "<div class='entity_title'>" + feature.type + " " + link + "</div>";
+    if (feature.tags['name']) {
+        infoHtml += "<div class='entity_title'>" + feature.tags['name'] + "</div>";
+    }
     for (var key in feature.tags) {
-       infoHtml += "<tr><td>" + key + "</td><td>" + feature.tags[key] + "</td></tr>";
+        infoHtml += "<tr><td>" + key + "</td><td>" + feature.tags[key] + "</td></tr>";
     }
     infoHtml += "</table>";
     infoHtml += "<div class='entity_meta'>" + new Date(feature.timestamp).toLocaleString() + " &middot; v" + feature.version + "</div>";
